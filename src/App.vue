43d149d232690371167065c8034b57ce6c4afa37
EchoCow <lizhongyue248@163.com>
2020-01-04 10:59:25 +0800

    :heavy_plus_sign: Add vuetify.

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,61 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">{{$t("message")}}}</router-link>
-      <button @click="test">test</button>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
+import Vue from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
 
-@Component
-export default class App extends Vue {
-  created () {
-    this.$i18n.locale = 'en'
-  }
-  test () {
-    this.$i18n.locale = this.$i18n.locale === 'en' ? 'zh' : 'en'
-  }
-}
+export default Vue.extend({
+  name: 'App',
+
+  components: {
+    HelloWorld
+  },
+
+  data: () => ({
+    //
+  })
+})
 </script>
-
-<style lang="stylus">
-#app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
-</style>
