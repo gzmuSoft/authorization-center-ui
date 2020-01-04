12180b5226ec51b94ae04c0bc89232dcc73b01d3
EchoCow <lizhongyue248@163.com>
2020-01-04 10:57:57 +0800

    :white_check_mark: Tests i18n.

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,26 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">{{$t("message")}}}</router-link>
+      <button @click="test">test</button>
     </div>
     <router-view/>
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class App extends Vue {
+  created () {
+    this.$i18n.locale = 'en'
+  }
+  test () {
+    this.$i18n.locale = this.$i18n.locale === 'en' ? 'zh' : 'en'
+  }
+}
+</script>
 
 <style lang="stylus">
 #app
